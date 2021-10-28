060949f6d355e98531f644e08c7ae7420c0cdf09
Nam <nam.mirbeau@gmail.com>
2021-10-28 21:04:41 +0200

    projet

---
+++ TPReseau3/TPReseau3/TPReseau3.cpp
@@ -0,0 +1,12 @@
+﻿// TPReseau3.cpp : définit le point d'entrée de l'application.
+//
+
+#include "TPReseau3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
