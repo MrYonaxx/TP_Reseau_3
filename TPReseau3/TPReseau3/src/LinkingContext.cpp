8988409b33b2c6bbbbca319822df392f9af767fb
Begin Florian <misterluckyyt@gmail.com>
2021-11-02 22:58:28 -0400

    Creation ClientServer

--- TPReseau3/TPReseau3/src/LinkingContext.cpp
+++ TPReseau3/TPReseau3/src/LinkingContext.cpp
@@ -3,39 +3,37 @@
 
 namespace uqac::replication
 {
-
-	void AddGameObject(int val, GameObject* object)
+	void LinkingContext::AddGameObject(int val, GameObject* object)
 	{
 		GameObject1.insert(val, object);
 		GameObject2.insert(object, val);
 	};
 
-	void DeleteGameObject(GameObject* object)
+	void LinkingContext::DeleteGameObject(GameObject* object)
 	{
 		it = GameObject2.find(object);
 		GameObject1.erase(it);
 		GameObject2.erase(object);
 	};
 
-	void AddGameObject(GameObject* object)
+	void LinkingContext::AddGameObject(GameObject* object)
 	{
 		it = GameObject2.end() + 1;
 		GameObject1.insert(it, object);
 		GameObject.insert(object, it);
 	};
 
-	std::optional<int> getObject1(GameObject* object)
+	std::optional<int> LinkingContext::getObject1(GameObject* object)
 	{
 		it = GameObject2.find(object);
 
 		return it;
 	};
 
-	std::optional<GameObject*> getObject1(int val)
+	std::optional<GameObject*> LinkingContext::getObject1(int val)
 	{
 		it = GameObject1.find(val);
 
 		return it;
 	}
-
 }
